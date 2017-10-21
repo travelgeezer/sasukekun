fed72c5064665199760f12406e9f9c4869887d5f
travelgeezer <travelgeezer@icloud.com>
2017-10-21 16:43:18 +0800

    add banner api

--- crawler/views.py
+++ crawler/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 # Create your views here.
 
+from .models import crawler_news, find_banner_node
+
+
+def response_json(data):
+    response = {
+      'code': 0,
+      'data': data,
+      'info': 'ok!'
+    }
+    return json.dumps(response, ensure_ascii=False)
+
 def index(request):
-        return HttpResponse('Hello, crawler')
+    return HttpResponse('hello carwler')
+
+def banner(request):
+    soup = crawler_news()
+    banner = soup.find_all('div', 'slide-item')
+    banner_datas = find_banner_node(banner)
+    return HttpResponse(response_json(banner_datas))
